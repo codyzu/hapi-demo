b95239cfd3d1c61c10c1732afd8144d5aaa11e11
codyzu <cody.zuschlag@gmail.com>
2016-05-25 17:48:26 +0200

    add get by name + joi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import hapi from 'hapi'
+import joi from 'joi'
 
 const server = new hapi.Server()
 server.connection({port: 3000})
@@ -8,6 +9,21 @@ server.route({
   path: '/',
   handler: (request, reply) => {
     reply('Hello world!')
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, reply) => {
+    console.log(`hello ${request.params.name}!`)
+  },
+  config: {
+    validate: {
+      params: {
+        name: joi.string().max(10)
+      }
+    }
   }
 })
 

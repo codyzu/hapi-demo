6b370f073264a972615569a960f8f58391d520c9
codyzu <cody.zuschlag@gmail.com>
2016-05-28 22:20:02 +0200

    add blip and good plugins

--- config.js
+++ config.js
@@ -9,6 +9,26 @@ export default {
 
   // hapi plugin registrations
   registrations: [
+    {
+      plugin: {
+        register: 'good',
+        options: {
+          ops: {interval: 1000},
+          reporters: {
+            console: [
+              {
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ log: '*', response: '*', error: '*', request: '*' }]
+              },
+              {module: 'good-console'},
+              'stdout'
+            ]
+          }
+        }
+      }
+    },
+    {plugin: 'blipp'},
     {plugin: './orgs'}
   ]
 }

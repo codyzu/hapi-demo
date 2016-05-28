42db826a7a3047b45c379a677bcce9aef93a85a9
codyzu <cody.zuschlag@gmail.com>
2016-05-28 22:02:47 +0200

    create plugin (fix logging)

--- index.js
+++ index.js
@@ -1,62 +1,19 @@
 import hapi from 'hapi'
-import joi from 'joi'
+import orgs from './orgs'
 
 const server = new hapi.Server()
 server.connection({port: 3000})
 
-const db = {}
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    const orgs = Object.values(db)
-    console.log('GET ALL:', orgs)
-    reply(orgs)
-  }
-})
-
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: (request, reply) => {
-    console.log('GET', request.params.name, db[request.params.db])
-    reply(db[request.params.name])
-  },
-  config: {
-    validate: {
-      params: {
-        name: joi.string().max(10)
-      }
-    }
-  }
-})
-
-server.route({
-  method: 'POST',
-  path: '/',
-  handler: (request, reply) => {
-    console.log('POST', request.payload)
-    db[request.payload.name] = request.payload
-    reply(request.payload).code(201)
-  },
-  config: {
-    validate: {
-      payload: joi.object({
-        name: joi.string().max(10).required(),
-        email: joi.string().email().required()
-      }).unknown(),
-      options: {
-        stripUnknown: true
-      }
-    }
-  }
-})
-
-server.start((err) => {
+server.register(orgs, (err) => {
   if (err) {
     throw err
   }
 
-  console.log(`Server running at: ${server.info.uri}`)
+  server.start((err) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`Server running at: ${server.info.uri}`)
+  })
 })
---
+++ orgs/index.js
@@ -0,0 +1,60 @@
+import joi from 'joi'
+import pack from '../package.json'
+
+const db = {}
+
+export default function register (server, options, next) {
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      const orgs = Object.values(db)
+      console.log('GET ALL:', orgs)
+      reply(orgs)
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, reply) => {
+      console.log('GET', request.params.name, db[request.params.name])
+      reply(db[request.params.name])
+    },
+    config: {
+      validate: {
+        params: {
+          name: joi.string().max(10)
+        }
+      }
+    }
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/',
+    handler: (request, reply) => {
+      console.log('POST', request.payload)
+      db[request.payload.name] = request.payload
+      reply(request.payload).code(201)
+    },
+    config: {
+      validate: {
+        payload: joi.object({
+          name: joi.string().max(10).required(),
+          email: joi.string().email().required()
+        }).unknown(),
+        options: {
+          stripUnknown: true
+        }
+      }
+    }
+  })
+
+  next()
+}
+
+register.attributes = {
+  name: 'orgs',
+  version: pack.version
+}

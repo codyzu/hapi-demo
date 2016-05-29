966dc01d553008f458fa725c255b88caa1a998cd
codyzu <cody.zuschlag@gmail.com>
2016-05-29 22:09:28 +0200

    add orgs model

---
+++ models/orgs.js
@@ -0,0 +1,30 @@
+import PouchDb from 'pouchdb'
+import {defaultsDeep} from 'lodash'
+
+export default class Orgs {
+  constructor () {
+    this.db = new PouchDb('orgdb')
+  }
+
+  createOrg (org) {
+    const orgDoc = defaultsDeep({_id: `orgs/${org.name}`}, org)
+
+    return this.db.put(orgDoc)
+
+    .then(() => orgDoc)
+  }
+
+  getOrg (name) {
+    return this.db.get(`orgs/${name}`)
+  }
+
+  listOrgs () {
+    return this.db.allDocs({
+      startkey: 'orgs/',
+      endkey: 'orgs/\uffff',
+      include_docs: true
+    })
+
+    .then((results) => results.rows.map((r) => r.doc))
+  }
+}

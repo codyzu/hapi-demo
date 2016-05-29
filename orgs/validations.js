b316f64d3bbb1e86955939f966d82e97c27e3f10
codyzu <cody.zuschlag@gmail.com>
2016-05-29 15:48:57 +0200

    add examples to schema

--- orgs/validations.js
+++ orgs/validations.js
@@ -1,8 +1,8 @@
 import joi from 'joi'
 
 export const org = joi.object({
-  name: joi.string().max(10).required(),
-  email: joi.string().email().required()
+  name: joi.string().max(10).required().example('axway'),
+  email: joi.string().email().required().example('cody@email.com')
 }).unknown()
 
-export const orgList = joi.array().items(org)
+export const orgList = joi.array().items(org).example([{name: 'dfdf', email: 'sdfds@dfd.com'}])

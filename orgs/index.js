cc399090afca6ecaae7e53728b2f767693dc0e8a
codyzu <cody.zuschlag@gmail.com>
2016-05-29 15:56:15 +0200

    move handlers inside config

--- orgs/index.js
+++ orgs/index.js
@@ -8,15 +8,15 @@ export function register (server, options, next) {
   server.route({
     method: 'GET',
     path: '/',
-    handler: (request, reply) => {
-      const orgs = Object.values(db)
-      console.log('GET ALL:', orgs)
-      reply(orgs)
-    },
     config: {
       tags: ['api'],
       response: {
         schema: validations.orgList
+      },
+      handler: (request, reply) => {
+        const orgs = Object.values(db)
+        console.log('GET ALL:', orgs)
+        reply(orgs)
       }
     }
   })
@@ -24,10 +24,6 @@ export function register (server, options, next) {
   server.route({
     method: 'GET',
     path: '/{name}',
-    handler: (request, reply) => {
-      console.log('GET', request.params.name, db[request.params.name])
-      reply(db[request.params.name])
-    },
     config: {
       tags: ['api'],
       validate: {
@@ -35,18 +31,17 @@ export function register (server, options, next) {
           name: joi.string().max(10)
         }
       },
-      response: {schema: validations.org}
+      response: {schema: validations.org},
+      handler: (request, reply) => {
+        console.log('GET', request.params.name, db[request.params.name])
+        reply(db[request.params.name])
+      }
     }
   })
 
   server.route({
     method: 'POST',
     path: '/',
-    handler: (request, reply) => {
-      console.log('POST', request.payload)
-      db[request.payload.name] = request.payload
-      reply(request.payload).code(201)
-    },
     config: {
       tags: ['api'],
       validate: {
@@ -55,7 +50,12 @@ export function register (server, options, next) {
           stripUnknown: true
         }
       },
-      response: {schema: validations.org}
+      response: {schema: validations.org},
+      handler: (request, reply) => {
+        console.log('POST', request.payload)
+        db[request.payload.name] = request.payload
+        reply(request.payload).code(201)
+      }
     }
   })
 

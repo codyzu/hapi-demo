1236fa47306eeeea950a94507afb3412f3e715ad
codyzu <cody.zuschlag@gmail.com>
2016-05-31 14:28:24 +0200

    test orgs model in index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import config from './config'
 import glue from 'glue'
 
+import Orgs from './models/orgs'
+
+const orgs = new Orgs()
+orgs.createOrg({name: 'Axway', desc: 'software', email: 'cody@zusch.com'})
+.then((doc) => console.log('NEW ORG:', doc))
+.then(() => orgs.getOrg('Axway'))
+.then((doc) => console.log('EXISTING ORG:', doc))
+.then(() => orgs.listOrgs())
+.then((docs) => console.log('EXISTING ORGS:', docs))
+.catch((err) => console.log('ERR:', err))
+
 glue.compose(config, {relativeTo: __dirname}, (err, server) => {
   if (err) {
     throw err

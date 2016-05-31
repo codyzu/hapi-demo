90169086ed9fa0add3f4171ac45da5a499c14a18
codyzu <cody.zuschlag@gmail.com>
2016-05-31 15:57:40 +0200

    back to memory storage

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ export default {
       }
     },
     {plugin: 'blipp'},
-    {plugin: {register: './models', options: {connection: {name: './orgsdb'}}}},
+    {plugin: {register: './models'}},
     {
       plugin: './orgs',
       options: {

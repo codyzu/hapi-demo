8bc0d06c609a9f9d490b12a1ab6237df9cfa4953
codyzu <cody.zuschlag@gmail.com>
2016-05-31 15:56:47 +0200

    show persistant storage

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ export default {
       }
     },
     {plugin: 'blipp'},
-    {plugin: './models'},
+    {plugin: {register: './models', options: {connection: {name: './orgsdb'}}}},
     {plugin: './orgs'}
   ]
 }

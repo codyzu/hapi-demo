7f789ef0e1e9c30744510553844bf2cdec540095
codyzu <cody.zuschlag@gmail.com>
2016-06-05 14:26:00 +0200

    extract plugin handlers

---
+++ orgs/handlers.js
@@ -0,0 +1,19 @@
+const db = {}
+
+export function getAllOrgs (request, reply) {
+  const orgs = Object.values(db)
+  console.log('GET ALL:', orgs)
+  reply(orgs)
+}
+
+export function getOrgByName (request, reply) {
+  const org = db[request.params.name]
+  console.log('GET', request.params.name, org)
+  reply(org)
+}
+
+export function postOrg (request, reply) {
+  console.log('POST', request.payload)
+  db[request.payload.name] = request.payload
+  reply(request.payload).code(201)
+}

36b4a6b9324efdb3af2974fc195268c73dde9e60
codyzu <cody.zuschlag@gmail.com>
2016-06-05 17:44:57 +0200

    add url to validations+schema

--- orgs/validations.js
+++ orgs/validations.js
@@ -2,9 +2,13 @@ import joi from 'joi'
 
 export const name = joi.string().max(10).required().example('Axway').default('Axway')
 
-export const org = joi.object({
+export const newOrg = joi.object({
   name,
   email: joi.string().email().required().example('cody@email.com')
 }).unknown()
+
+export const org = newOrg.concat(joi.object({
+  url: joi.string().uri().example('http://server.com/orgs/Axway')
+}))
 
 export const orgList = joi.array().items(org).example([{name: 'Axway', email: 'cody@email.com'}])

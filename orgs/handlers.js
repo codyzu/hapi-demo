4e0b970c6545e138c0976b226aa8d48bec4ba547
codyzu <cody.zuschlag@gmail.com>
2016-06-05 17:09:12 +0200

    refactor handlers using small pre handlers

--- orgs/handlers.js
+++ orgs/handlers.js
@@ -1,51 +1,40 @@
 import boom from 'boom'
 import {defaultsDeep, omit} from 'lodash'
 
-export function getAllOrgs (request, reply) {
+export function fetchOrg (request, reply) {
+  const orgName = request.params.name
+  console.log('FETCH:', orgName)
+  request.models.orgs.getOrg(orgName)
+  .then((org) => {
+    reply([org])
+  })
+  .catch((err) => reply(boom.wrap(err, err.status)))
+}
+
+export function fetchAllOrgs (request, reply) {
+  console.log('FETCH ALL')
   request.models.orgs.listOrgs()
   .then((orgs) => {
-    console.log('GET ALL:', orgs)
     reply(orgs)
   })
   .catch((err) => reply(boom.wrap(err, err.status)))
 }
 
-export function getOrgByName (request, reply) {
-  const orgName = request.params.name
-  request.models.orgs.getOrg(orgName)
-  .then((org) => {
-    console.log('GET:', org)
-    reply(org)
-  })
-  .catch((err) => reply(boom.wrap(err, err.status)))
-}
-
-export function postOrg (request, reply) {
+export function createOrg (request, reply) {
+  console.log('CREATING')
   request.models.orgs.createOrg(request.payload)
   .then((org) => {
-    console.log('CREATED:', org)
-    reply(org).code(201)
+    reply([org]).code(201)
   })
   .catch((err) => reply(boom.wrap(err, err.status)))
-}
-
-export function fetchOrg (request, reply) {
-
-}
-
-export function fetchAllOrgs (request, reply) {
-
-}
-
-export function createOrg (request, reply) {
-
 }
 
 export function prepareOrgs (request, reply) {
   console.log('PREPARING')
-  reply(defaultsDeep({url: `${request.server.info.uri}/${request.pre.org._id}`}, omit(request.pre.org, ['_id', '_rev'])))
+  reply(request.pre.orgs.map((org) => defaultsDeep({url: `${request.server.info.uri}/${org._id}`}, omit(org, ['_id', '_rev']))))
 }
 
 export function firstOrg (request, reply) {
-
+  console.log('FIRST ORG')
+  reply(request.pre.orgs[0])
 }
